7e2d95b9b8142deea827fa3f91c6499d5ed2a35f
Josh Rimes - Desktop <joshjohn1104@gmail.com>
2025-06-27 16:11:55 +0100

    Implemented error catching (404, 500, other) for function fetchPokemon

--- pokemon-explorer-challenge/frontend/pages/index.js
+++ pokemon-explorer-challenge/frontend/pages/index.js
@@ -14,16 +14,33 @@ export default function Home() {
    * 4. Handle error state (Pokemon not found, network errors, etc.)
    */
   const fetchPokemon = async () => {
-      const cleanedQuery = query.trim().toLowerCase();
+      const cleanedQuery = query.trim().toLowerCase(); // User input is cleaned so that the name passed into the backend is consistent with the PokeAPI's URL
       if (!cleanedQuery) return;
 
+      // React states set before fetch is called
       setLoading(true);
       setError(null);
       setPokemon(null);
 
       try {
-          const response = await fetch(`http://localhost:3001/api/pokemon/${cleanedQuery}`);
-          throw new Error("Placeholder error.");
+          const response = await fetch(`http://localhost:3001/api/pokemon/${cleanedQuery}`); // Attempts to access backend GET route
+          
+          // If response status is an error then send an error message
+          if (!response.ok)
+          {
+              if (response.status === 404)
+              {
+                  throw new Error(`Pokemon "${cleanedQuery}" not found.`);
+              }
+              else if (response.status === 500)
+              {
+                  throw new Error('Internal Server Error.');
+              }
+              else 
+              {
+                  throw new Error('Failed to fetch Pokemon data.');
+              }
+          }
       }
       catch (err)
       {
