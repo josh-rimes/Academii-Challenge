d195533c03fe14fbd59b37a1d470d6beeb596b48
Josh Rimes - Desktop <joshjohn1104@gmail.com>
2025-06-27 15:45:44 +0100

    Implemented GET /api/pokemon/:name to fetch and return filtered Pokemon data

--- pokemon-explorer-challenge/backend/routes/pokemon.js
+++ pokemon-explorer-challenge/backend/routes/pokemon.js
@@ -16,8 +16,44 @@ const router = express.Router();
  * 4. Handle errors appropriately (404 for Pokemon not found, 500 for server errors)
  */
 router.get("/:name", async (req, res) => {
-  // TODO: Implement this route handler
-  res.status(501).json({ error: "Not implemented" });
+    const { name } = req.params;
+    const APIURL = `https://pokeapi.co/api/v2/pokemon/${name.toLowerCase()}`; // .toLowerCase() converts any uppercase characters to lowercase
+
+    try {
+        const response = await fetch(APIURL); // Attempts to access the PokeAPI
+
+        
+        // If response status is an error then send an error message
+        if (!response.ok)
+        {
+            if (response.status == 400)
+            {
+              return res.status(400).json({ error: 'API error: bad request.' });
+            }
+            else if (response.status == 404) // Checks for 404 not found error
+            {
+                return res.status(404).json({ error: `Pokemon "${name}" not found.` });
+            }
+            else
+            {
+              return res.status(500).json({ error: 'Failed to fetch Pokemon data.' });
+            }
+        }
+
+        const data = await response.json(); // response.json holds all the data for the searched Pokemon
+        const filteredData = {
+            name: data.name,
+            sprite: data.sprites.front_default,
+            types: data.types.map((t) => t.type.name), // Forms a list of Pokemon types
+        };
+
+        res.json(filteredData); // The response data is now filtered using the filteredData json format
+    }
+    catch (err)
+    {
+        console.error('Server error:', err);
+        res.status(500).json({ error: 'Internal server error.' });
+    }
 });
 
 export default router;
